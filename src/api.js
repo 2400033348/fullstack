8b542a03909b79ba01e35a2ccbce9907bf533136
2400033348 <2400033348@kluniversity.in>
2026-04-07 21:15:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://backend-production-0919.up.railway.app/api";
 
 const parseJsonResponse = async (response) => {
   const data = await response.json().catch(() => ({}));
